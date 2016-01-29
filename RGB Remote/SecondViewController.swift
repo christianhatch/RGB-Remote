97db44e058d55df66081ae4d4211957ab3d6436f
Christian Hatch <christianhatch@gmail.com>
2016-01-28 19:04:28 -0500

    Initial Commit

---
+++ RGB Remote/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RGB Remote
+//
+//  Created by Christian Hatch on 1/28/16.
+//  Copyright © 2016 Knot Labs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
