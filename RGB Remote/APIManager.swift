5c4f73c4678c2e1b2e8eda6407fdfcb705dc3188
Christian Hatch <christianhatch@gmail.com>
2016-03-17 23:30:57 -0400

    Adding more commands

--- RGB Remote/APIManager.swift
+++ RGB Remote/APIManager.swift
@@ -9,9 +9,21 @@
 import Foundation
 import Alamofire
 
+
+
+
 enum Command: Int {
     
     static let all: [Command] = [.On, .Off, .BrightnessUp, .BrightnessDown, .Red, .Green, .Blue, .White, .WhiteOn, .WhiteOff, .WhiteUp, .WhiteDown, .White25, .White25, .White75, .White100, .Jump3, .Jump7, .Fade3, .Fade7, .Flash, .Auto, .Quick, .Slow]
+    
+    static let rgbww: [Command] = [.On, .White, .Blue, .Green, .Red,
+                                   .OrangeYellow, .YellowOrange, .YellowGreen, .GreenYellow,
+                                   .TealBlue, .SkyBlue, .IndigoBlue, .LightBlue,
+                                   .Indigo, .Violet, .Purple, .DeepPurple,
+                                   .PinkWhite, .PurpleWhite, .TealWhite, .IndigoWhite,
+                                   .DIY1, .DIY2, .DIY3, .DIY4, .DIY5, .DIY6,
+                                   .RedUp, .RedDown, .GreenUp, .GreenDown, .BlueUp, .BlueDown]
+    static let rgb: [Command] = []
     
     case On = 1
     case Off
@@ -25,7 +37,6 @@ enum Command: Int {
     
     case WhiteOn 
     case WhiteOff //10
-    
     case WhiteUp  //11
     case WhiteDown
     case White25 
@@ -39,11 +50,68 @@ enum Command: Int {
     case Fade3 //19
     case Fade7 
     
-    case Flash 
+    case Flash //21
     case Auto
     
-    case Quick 
+    case Quick //23
     case Slow
+    
+    //rgbww
+    case RedOrange
+    case Orange
+    case OrangeYellow
+    case Yellow
+    
+    case MossGreen
+    case Turquoise
+    case LightBlueWW
+    case SkyBlueWW
+   
+    case UV
+    case IndigoWW
+    case VioletWW
+    case PurpleWW
+    
+    case PinkWhiteWW
+    case PurpleWhiteWW
+    case TealWhiteWW
+    case IndigoWhiteWW
+    
+    
+    //rgb
+    case RichOrange
+    case YellowOrange
+    case YellowGreen
+    case GreenYellow
+    
+    case TealBlue
+    case SkyBlue
+    case IndigoBlue
+    case LightBlue
+
+    case Indigo
+    case Violet
+    case Purple
+    case DeepPurple
+    
+    case PinkWhite
+    case PurpleWhite
+    case TealWhite
+    case IndigoWhite
+    
+    case DIY1
+    case DIY2
+    case DIY3
+    case DIY4
+    case DIY5
+    case DIY6
+
+    case RedUp
+    case RedDown
+    case GreenUp
+    case GreenDown
+    case BlueUp
+    case BlueDown
 }
 
 
@@ -106,6 +174,109 @@ extension Command {
             return "Faster"
         case .Slow:
             return "Slower"
+            
+            
+        //rgbww
+        case .RedOrange:
+            return "Red Orange"
+        case .Orange:
+            return "Orange"
+        case .OrangeYellow:
+            return "Orange Yellow"
+        case .Yellow:
+            return "Yellow"
+            
+        case .MossGreen:
+            return "Moss Green"
+        case .Turquoise:
+            return "Turquoise"
+//        case .LightBlue:
+//            return "Light Blue"
+//        case .SkyBlue:
+//            return "Sky Blue"
+            
+        case .UV:
+            return "UV"
+//        case .Indigo:
+//            return "Indigo"
+//        case .Violet:
+//            return "Violet"
+//        case .Purple:
+//            return "Purple"
+            
+//        case .PinkWhite:
+//            return "Pink White"
+//        case .PurpleWhite:
+//            return "Purple White"
+//        case .TealWhite:
+//            return "Teal White"
+//        case .IndigoWhite:
+//            return "Indigo White"
+
+            
+        //rgb
+        case .RichOrange:
+            return "Rich Orange"
+        case .YellowOrange:
+            return "Yellow Orange"
+        case .YellowGreen:
+            return "Yellow Green"
+        case .GreenYellow:
+            return "Green Yellow"
+            
+        case .TealBlue:
+            return "Teal Blue"
+        case .SkyBlue, .SkyBlueWW:
+            return "Sky Blue"
+        case .IndigoBlue:
+            return "Indigo Blue"
+        case .LightBlue, .LightBlueWW:
+            return "Light Blue"
+            
+        case .Indigo, .IndigoWW:
+            return "Indigo"
+        case .Violet, .VioletWW:
+            return "Violet"
+        case .Purple, .PurpleWW:
+            return "Purple"
+        case .DeepPurple:
+            return "Deep Purple"
+            
+        case .PinkWhite, .PinkWhiteWW:
+            return "Pink White"
+        case .PurpleWhite, .PurpleWhiteWW:
+            return "Purple White"
+        case .TealWhite, .TealWhiteWW:
+            return "Teal White"
+        case .IndigoWhite, .IndigoWhiteWW:
+            return "Indigo White"
+            
+        case .DIY1:
+            return "DIY 1"
+        case .DIY2:
+            return "DIY 2"
+        case .DIY3:
+            return "DIY 3"
+        case .DIY4:
+            return "DIY 4"
+        case .DIY5:
+            return "DIY 5"
+        case .DIY6:
+            return "DIY 6"
+            
+        case .RedUp:
+            return "Red Up"
+        case .RedDown:
+            return "Red Down"
+        case .GreenUp:
+            return "Green Up"
+        case .GreenDown:
+            return "Green Down"
+        case .BlueUp:
+            return "Blue Up"
+        case .BlueDown:
+            return "Blue Down"
+
         }
     }
 
@@ -116,58 +287,60 @@ extension Command {
     func apiKey() -> String {
         switch self {
         case .On:
-            return "KEY_ON"
+            return "on"
         case .Off:
-            return "KEY_OFF"
+            return "off"
         case .BrightnessUp:
-            return "KEY_BRIGHTNESSUP"
+            return "up"
         case .BrightnessDown:
-            return "KEY_BRIGHTNESSDOWN"
+            return "down"
         case .Red:
-            return "KEY_RED"
+            return "red"
         case .Green:
-            return "KEY_GREEN"
+            return "green"
         case .Blue:
-            return "KEY_BLUE"
+            return "blue"
         case .White:
-            return "KEY_WHITE"
+            return "white"
             
         case .WhiteOn:
-            return "KEY_WHITEON"
+            return "white_on"
         case .WhiteOff:
-            return "KEY_WHITEOFF"
+            return "white_on"
         case .WhiteUp:
-            return "KEY_WHITEUP"
+            return "white_up"
         case .WhiteDown:
-            return "KEY_WHITEDOWN"
+            return "white_down"
         case .White25:
-            return "KEY_WHITE25"
+            return "white_25"
         case .White50:
-            return "KEY_WHITE50"
+            return "white_50"
         case .White75:
-            return "KEY_WHITE75"
+            return "white_75"
         case .White100:
-            return "KEY_WHITE100"
+            return "white_100"
             
         case .Jump3:
-            return "KEY_JUMP3"
+            return "jump_3"
         case .Jump7:
-            return "KEY_JUMP7"
+            return "jump_7"
             
         case .Fade3:
-            return "KEY_FADE3"
+            return "fade_3"
         case .Fade7:
-            return "KEY_FADE7"
+            return "fade_7"
             
         case .Flash:
-            return "KEY_FLASH"
+            return "flash"
         case .Auto:
-            return "KEY_AUTO"
+            return "auto"
             
         case .Quick:
-            return "KEY_QUICK"
+            return "quick"
         case .Slow:
-            return "KEY_SLOW"
+            return "slow"
+            
+            
         }
     }
     
