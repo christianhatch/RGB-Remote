97689b026a7e2e520df0dd000b1d1ab91ff193f1
Christian Hatch <christianhatch@gmail.com>
2016-03-24 02:58:19 -0400

    Extract datasource.

--- RGB Remote/FirstViewController.swift
+++ RGB Remote/FirstViewController.swift
@@ -9,16 +9,6 @@
 import UIKit
 import ChameleonFramework
 
-private enum SectionType {
-    case BasicControls
-    case BasicColors
-    case WWControls
-    case RGBWWColors
-}
-private struct Section {
-    var type: SectionType
-    var items: [Command]
-}
 
 class FirstViewController: UIViewController {
 
@@ -26,10 +16,7 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var collectionViewHeight: NSLayoutConstraint!
     
-    private var collectionViewSections: [Section] = [Section(type: .RGBWWColors, items: Command.rgbwwColors),
-                                                     Section(type: .WWControls, items: Command.wwControls),
-                                                     Section(type: .BasicControls, items: Command.basicControls),
-                                                     Section(type: .BasicColors, items: Command.basicColors)]
+    let dataSource = RGBWWDataSource()
 }
 
 //MARK: - UIKit
@@ -39,8 +26,11 @@ extension FirstViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.collectionView.registerNib(UINib(nibName: "ButtonCell", bundle: nil), forCellWithReuseIdentifier: "ButtonCell")
         collectionView.backgroundColor = view.backgroundColor
+
+        collectionView.dataSource = dataSource
+        collectionView.delegate = dataSource
+        dataSource.register(collectionView)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -51,103 +41,6 @@ extension FirstViewController {
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
-    }
-}
-
-//MARK: - IBActions
-
-extension FirstViewController {
-    
-    @IBAction func buttonTouchDown(sender: AnyObject) {
-        guard let tag = Command(rawValue: sender.tag) else { return }
-        APIManager.startSendingCommand(tag)
-    }
-    
-    @IBAction func buttonTouchUp(sender: AnyObject) {
-        guard let tag = Command(rawValue: sender.tag) else { return }
-        APIManager.stopSendingCommand(tag)
-    }
-    
-    
-    @IBAction func buttonTapped(sender: AnyObject) {
-        guard let tag = Command(rawValue: sender.tag) else { return }
-        APIManager.sendCommand(tag)
-    }
-    
-}
-
-
-//MARK: - UICollectionView DataSource 
-
-extension FirstViewController: UICollectionViewDataSource {
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ButtonCell", forIndexPath: indexPath) as! ButtonCell
-        let command = collectionViewSections[indexPath.section].items[indexPath.item]
-     
-        cell.button.setTitle(command.humanReadableDescription(), forState: .Normal)
-        cell.button.addTarget(self, action: #selector(FirstViewController.buttonTapped(_:)), forControlEvents: .TouchUpInside)
-        cell.button.tag = command.rawValue
-        cell.button.setTitleColor(command.color(cell.contentView.backgroundColor), forState: .Normal)
-        
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectionViewSections[section].items.count
-    }
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return collectionViewSections.count
-    }
-
-}
-
-extension FirstViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.bounds.width, height: 10)
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-
-        var desiredButtonsPerRow: CGFloat = 4
-        
-        let command = collectionViewSections[indexPath.section].items[indexPath.row]
-        switch command {
-        case .Red, .Green, .Blue:
-            desiredButtonsPerRow = 3
-            break
-        case .White:
-            desiredButtonsPerRow = 1
-            break
-        default:
-            desiredButtonsPerRow = 4
-            break
-        }
-        
-        return CGSize(width: collectionView.bounds.width / desiredButtonsPerRow, height: 50)
-    }
-}
-
-
-class ButtonCell: UICollectionViewCell {
-    @IBOutlet weak var button: UIButton!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        button.titleLabel?.numberOfLines = 2
-        button.titleLabel?.textAlignment = .Center
     }
 }
 
---
+++ RGB Remote/RGBWWDataSource.swift
@@ -0,0 +1,134 @@
+//
+//  RGBWWDataSource.swift
+//  RGB Remote
+//
+//  Created by Christian Hatch on 3/24/16.
+//  Copyright © 2016 Knot Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+//MARK: - DataSource
+
+class RGBWWDataSource: NSObject {
+    
+    private enum SectionType {
+        case BasicControls
+        case BasicColors
+        case WWControls
+        case RGBWWColors
+    }
+    
+    private struct Section {
+        var type: SectionType
+        var items: [Command]
+    }
+    
+    private var sections: [Section] = [Section(type: .RGBWWColors, items: Command.rgbwwColors),
+                                                     Section(type: .WWControls, items: Command.wwControls),
+                                                     Section(type: .BasicControls, items: Command.basicControls),
+                                                     Section(type: .BasicColors, items: Command.basicColors)]
+    
+    func register(collectionView: UICollectionView) {
+        collectionView.registerNib(UINib(nibName: "ButtonCell", bundle: nil), forCellWithReuseIdentifier: "ButtonCell")
+    }
+}
+
+//MARK: - IBActions
+
+extension RGBWWDataSource {
+    
+//    func buttonTouchDown(sender: AnyObject) {
+//        guard let tag = Command(rawValue: sender.tag) else { return }
+//        APIManager.startSendingCommand(tag)
+//    }
+//    
+//    func buttonTouchUp(sender: AnyObject) {
+//        guard let tag = Command(rawValue: sender.tag) else { return }
+//        APIManager.stopSendingCommand(tag)
+//    }
+    
+    
+    func buttonTapped(sender: AnyObject) {
+        guard let tag = Command(rawValue: sender.tag) else { return }
+        APIManager.sendCommand(tag)
+    }
+    
+}
+
+//MARK: - UICollectionView DataSource
+
+extension RGBWWDataSource: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ButtonCell", forIndexPath: indexPath) as! ButtonCell
+        let command = sections[indexPath.section].items[indexPath.item]
+        
+        cell.button.setTitle(command.humanReadableDescription(), forState: .Normal)
+        cell.button.addTarget(self, action: #selector(RGBWWDataSource.buttonTapped(_:)), forControlEvents: .TouchUpInside)
+        cell.button.tag = command.rawValue
+        cell.button.setTitleColor(command.color(cell.contentView.backgroundColor), forState: .Normal)
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+}
+
+extension RGBWWDataSource: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 10)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        var desiredButtonsPerRow: CGFloat = 4
+        
+        let command = sections[indexPath.section].items[indexPath.item]
+        switch command {
+        case .Red, .Green, .Blue:
+            desiredButtonsPerRow = 3
+            break
+        case .White:
+            desiredButtonsPerRow = 1
+            break
+        default:
+            desiredButtonsPerRow = 4
+            break
+        }
+        
+        let size = CGSize(width: collectionView.bounds.width / desiredButtonsPerRow, height: 50)
+        return size
+    }
+}
+
+
+class ButtonCell: UICollectionViewCell {
+    @IBOutlet weak var button: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        button.titleLabel?.numberOfLines = 2
+        button.titleLabel?.textAlignment = .Center
+    }
+}
